3a4d1c995850efc723872bcb41d5ffdfa0fc1cb9
Luiz Augusto <gasparsurfpe@gmail.com>
2023-03-20 14:25:34 -0300

    20/03/2023
    Atualização inicial

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang=“pt-br”>
+<head>
+    <meta charset=“utf-8”/>
+    <meta content=“width=device-width, initial-scale=1, maximum-scale=1” name=“viewport”>
+    <title>Sistema de Gestão de Pedidos</title>
+ <link rel="stylesheet" href="Assents/css/bootstrap.css">
+    <link rel="stylesheet" href="Assents/css/estilo.css">
+</head>
+<body>
+<section>
+    <div class="container">
+        <form action="" method="post">
+            <label for="user">usuário</label>
+            <input type="text" name="user" id="user" class="form-control" placeholder="informe seu usuário"><p></p>
+
+            <label for="pw">senha</label>
+            <input type="password" name="pw" id="pw" class="form-control" placeholder="informe sua senha"><b></b>
+        </form>
+    </div>
+</section>
+
+</body>
+</html>
