06125321a91d26f213fd3adab0b3c0c1d9448a01
Luiz Augusto <gasparsurfpe@gmail.com>
2023-03-22 12:22:31 -0300

    22/03/2023
    Novos dados e nova atualizações

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 <html lang=“pt-br”>
 <head>
     <meta charset=“utf-8”/>
-    <meta content=“width=device-width, initial-scale=1, maximum-scale=1” name=“viewport”>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sistema de Gestão de Pedidos</title>
  <link rel="stylesheet" href="Assents/css/bootstrap.css">
     <link rel="stylesheet" href="Assents/css/estilo.css">
 </head>
 <body>
-
 <section>
     <div class="container">
+        <header>  <header><img src="" width="500" height="80" alt="logo" id="imgpos" /></header>
         <form action="" method="post">
             <label for="user">usuário</label>
             <input type="text" name="user" id="user" class="form-control" placeholder="informe seu usuário"><p></p>
@@ -18,10 +18,9 @@
             <label for="pw">senha</label>
             <input type="password" name="pw" id="pw" class="form-control" placeholder="informe sua senha"><p></p>
 
-            <button type="submit" class="btn btn btn-primary">Acessa</button>
+            <button type="submit" class="btn btn-primary">Acessar</button>
         </form>
     </div>
 </section>
-
 </body>
 </html>
